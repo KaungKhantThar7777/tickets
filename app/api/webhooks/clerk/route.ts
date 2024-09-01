a8af90fc689f789e73e83f6bfd0842c09c41b729
kaungkhantthar <kaungkhantthar77@gmail.com>
2024-09-01 22:26:37 +0700

    feat: added authentication with clerk

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,27 @@
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { prisma } from '@/utils/db/prisma'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const payload: WebhookEvent = await request.json()
+
+  if (payload.type === 'user.created') {
+    const { id, first_name, last_name, image_url } = payload.data
+
+    try {
+      await prisma.user.create({
+        data: {
+          id,
+          name: `${first_name} ${last_name}`,
+          image: image_url,
+        },
+      })
+
+      return NextResponse.json({ message: 'Success' })
+    } catch (error) {
+      return NextResponse.json({ message: 'Success' }, { status: 500 })
+    }
+  } else {
+    return NextResponse.json({ message: 'Bad Request' }, { status: 400 })
+  }
+}
